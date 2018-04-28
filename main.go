0bb1acd9d320340b45e0e6954e8aa1f8f9a97a48
chinanf-boy <865501259@qq.com>
2018-04-28 15:37:33 +0800

    Add bad-serve

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
+//go:generate fileb0x filebox.json
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/chinanf-boy/vue-with-go-example/server/cmd"
 )
 
 func main() {
+	cmd.Paths, _ = filepath.Abs(filepath.Dir(os.Args[0]))
 	cmd.Execute()
 }
